c7964d6eff36c6f57a14763114919c7024b53b2b
Robin Hassler <robin.edith74@gmail.com>
2023-10-11 16:44:54 +0200

    Added config file as source of needed URLs

--- API/database.py
+++ API/database.py
@@ -1,9 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from Utils import FileOperations
 
 
-DB_URL = "postgresql://user:password@postgresserver/web2_db"          #TODO: change to own database url
+# Use this variable to access the configuration file via the key of the variable
+config = FileOperations.read_json("config.json")
+DB_URL = config["ConnectionString"]          #TODO: change to own database url
 
 engine = create_engine(DB_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
--- API/main.py
+++ API/main.py
@@ -2,8 +2,8 @@ from fastapi import Depends, FastAPI, HTTPException
 from sqlalchemy.orm import Session
 from Crud import *
 from Schemas import *
-from . import models
-from .database import SessionLocal, engine
+import models
+from database import SessionLocal, engine
 import uvicorn
 
 
@@ -14,7 +14,7 @@ app = FastAPI()
 
 def run_api():
     print("Starting HTTP server in run_api()", flush=True)
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="127.0.0.1", port=8000)
 
 
 def get_db():
@@ -23,3 +23,11 @@ def get_db():
         yield db
     finally:
         db.close()
+
+
+def main():
+    print("Hallo")
+
+
+if __name__ == "__main__":
+    main()
