be0451530fa9a0ba0ecc06fbe5ec13aa1f4d185e
Weh, Selina <weh@infosim.net>
2023-10-13 21:04:42 +0200

    added ConfigManager to database.py

--- API/database.py
+++ API/database.py
@@ -1,14 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from Utils import FileOperations
 
+from Utils.ConfigManager import ConfigManager
 
-# Use this variable to access the configuration file via the key of the variable
-config = FileOperations.read_json("config.json")
-DB_URL = config["ConnectionString"]
+#TODO: instantiate ConfigManager as a singleton avoiding circular imports (database.py <> setup.py)
+config_manager = ConfigManager()
 
-engine = create_engine(DB_URL)
+engine = create_engine(config_manager.db_url)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
