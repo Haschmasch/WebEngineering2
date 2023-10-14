5e3e3d980e1a720a4d6734abfbc9b5d4c8d0bf29
Weh, Selina <weh@infosim.net>
2023-10-14 15:31:38 +0200

    added routers for each endpoint, commented out methods with non-working dependencys (Schema relationships), changed get_db() to get_db in Endpoint Depends

--- API/Endpoints/Users.py
+++ API/Endpoints/Users.py
@@ -2,13 +2,14 @@ from API import setup_database
 from API.Crud import Users
 from API.Schemas import User
 from sqlalchemy.orm import Session
-from fastapi import FastAPI, HTTPException, Depends, status
+from fastapi import APIRouter, HTTPException, Depends, status
 from sqlalchemy import exc
-from API.setup_api import app
+
+router = APIRouter()
 
 
-@app.post("/users/", response_model=User.User, status_code=status.HTTP_201_CREATED)
-def register_user(user: User.UserCreate, db: Session = Depends(setup_database.get_db())):
+@router.post("/users/", response_model=User.User, status_code=status.HTTP_201_CREATED)
+def register_user(user: User.UserCreate, db: Session = Depends(setup_database.get_db)):
     try:
         return Users.create_user(db, user)
     except exc.DatabaseError as e:
@@ -16,8 +17,8 @@ def register_user(user: User.UserCreate, db: Session = Depends(setup_database.ge
 
 
 # TODO: Implement proper login (maybe with jwt tokens)
-@app.post("/users/login", response_model=User.User)
-def login_user(user: User.UserLogin, db: Session = Depends(setup_database.get_db())):
+@router.post("/users/login", response_model=User.User)
+def login_user(user: User.UserLogin, db: Session = Depends(setup_database.get_db)):
     try:
         user = Users.check_user_exists(db, user)
         if user is None:
@@ -27,44 +28,44 @@ def login_user(user: User.UserLogin, db: Session = Depends(setup_database.get_db
         raise HTTPException(status_code=400, detail=e.detail)
 
 
-@app.put("/users/", response_model=User.User)
-def update_user(user: User.User, db: Session = Depends(setup_database.get_db())):
+@router.put("/users/", response_model=User.User)
+def update_user(user: User.User, db: Session = Depends(setup_database.get_db)):
     try:
         return Users.update_user(db, user)
     except exc.DatabaseError as e:
         raise HTTPException(status_code=400, detail=e.detail)
 
 
-@app.get("/users/{user_id}", response_model=User.User)
-def get_user(user_id: int, db: Session = Depends(setup_database.get_db())):
+@router.get("/users/{user_id}", response_model=User.User)
+def get_user(user_id: int, db: Session = Depends(setup_database.get_db)):
     try:
         return Users.get_user(db, user_id)
     except exc.DatabaseError as e:
         raise HTTPException(status_code=400, detail=e.detail)
 
 
-@app.get("/users/name/{user_name}", response_model=User.User)
-def get_user_by_name(user_name: str, db: Session = Depends(setup_database.get_db())):
+@router.get("/users/name/{user_name}", response_model=User.User)
+def get_user_by_name(user_name: str, db: Session = Depends(setup_database.get_db)):
     try:
         return Users.get_user_by_name(db, user_name)
     except exc.DatabaseError as e:
         raise HTTPException(status_code=400, detail=e.detail)
 
-
+'''
 # TODO: Integrate relations for offers
-@app.get("/users/{user_id}/offers", response_model=User.UserWithOffers)
-def get_user_with_offers(user_id: int, db: Session = Depends(setup_database.get_db())):
+@router.get("/users/{user_id}/offers", response_model=User.UserWithOffers)
+def get_user_with_offers(user_id: int, db: Session = Depends(setup_database.get_db)):
     try:
         return Users.get_user(db, user_id)
     except exc.DatabaseError as e:
         raise HTTPException(status_code=400, detail=e.detail)
 
 
-@app.get("/users/", response_model=list[User.User])
-def get_users(skip: int = 0, limit: int = 100, db: Session = Depends(setup_database.get_db())):
+@router.get("/users/", response_model=list[User.User])
+def get_users(skip: int = 0, limit: int = 100, db: Session = Depends(setup_database.get_db)):
     try:
         return Users.get_users(db, skip, limit)
     except exc.DatabaseError as e:
         raise HTTPException(status_code=400, detail=e.detail)
 
-
+'''
--- API/setup_api.py
+++ API/setup_api.py
@@ -1,10 +1,16 @@
 from fastapi import Depends, FastAPI, HTTPException
 import uvicorn
 from API.Endpoints.Chats import router as chat_router
+from API.Endpoints.Search import router as search_router
+from API.Endpoints.Categories import router as category_router
+from API.Endpoints.Subcategories import router as subcategory_router
+from API.Endpoints.Users import router as user_router
+from API.Endpoints.Offers import router as offer_router
 
-
+routers = [chat_router, search_router, category_router, subcategory_router, user_router, offer_router]
 app = FastAPI()
-app.include_router(chat_router)
+for router in routers:
+    app.include_router(router)
 
 
 def run_api():
