4689e1f64076022777072c834e50a1c1135fc995
Weh, Selina <weh@infosim.net>
2023-10-16 14:07:10 +0200

    added all Chat endpoints and adapted cruds

--- API/Crud/Chats.py
+++ API/Crud/Chats.py
@@ -4,25 +4,41 @@ from sqlalchemy.orm import Session
 from sqlalchemy import insert, update, delete, select
 from API import models
 from API.Schemas import Chat
+from API.Utils.FileOperations import write_json, remove_file, get_chat_file_path
 from API.Utils.Exceptions import EntryNotFoundException
 
 
-def create_chat(db: Session, chat: Chat.ChatCreate):
+def create_chat(db: Session, chat: Chat.ChatCreate, chat_root_directory: str):
     db_chat = models.Chat(offer_id=chat.offer_id, creator_id=chat.creator_id,
                           time_opened=datetime.datetime.now(tz=datetime.timezone.utc).isoformat())
     db.add(db_chat)
     db.commit()
     db.refresh(db_chat)
+    try:
+        filepath = get_chat_file_path(db_chat.id, chat_root_directory)
+        write_json(filepath, [])
+    except Exception as e:
+        db.delete(db_chat)
+        db.commit()
+        raise Exception(f"Failed to create message history file. Error: {str(e)}")
     return db_chat
 
 
-def delete_chat(db: Session, chat_id: int):
+def delete_chat(db: Session, chat_id: int, chat_root_directory: str):
     chat = get_chat(db, chat_id)
+    try:
+        filepath = get_chat_file_path(chat_id, chat_root_directory)
+        remove_file(filepath)
+    except FileNotFoundError:
+        pass
+    except Exception as e:
+        print(f"Failed to delete message history file. Error: {str(e)}")
     if chat is not None:
         db.execute(delete(models.Chat).where(models.Chat.id == chat_id))
         db.commit()
     else:
         raise EntryNotFoundException(f"No database entry found for chat_id: {chat_id}")
+
 
 
 def update_chat(db: Session, chat: Chat.Chat):
--- API/Endpoints/Chats.py
+++ API/Endpoints/Chats.py
@@ -1,12 +1,17 @@
-from fastapi import WebSocket, APIRouter, Depends, WebSocketDisconnect
+from fastapi import WebSocket, APIRouter, Depends, WebSocketDisconnect, HTTPException, status
+from sqlalchemy import exc
 from sqlalchemy.orm import Session
 
+from API.Crud import Chats
+from API.Schemas import Chat
 from API.Websockets.ConnectionManager import ConnectionManager
 from API.setup_database import get_db
 from API.Utils.ConfigManager import configuration
 
 
-router = APIRouter()
+router = APIRouter(
+    prefix="/chats",
+    tags=["chats"])
 
 
 @router.websocket("/ws/{offerid}/{user_id}")
@@ -20,3 +25,73 @@ async def websocket_endpoint(websocket: WebSocket, offerid: str, db: Session = D
             await manager.broadcast(f"{data}", offerid)
     except WebSocketDisconnect:
         await manager.disconnect(websocket, offerid)
+
+
+@router.post("/", response_model=Chat.Chat, status_code=status.HTTP_201_CREATED)
+def add_chat(chat: Chat.ChatCreate, db: Session = Depends(get_db)):
+    try:
+        return Chats.create_chat(db, chat, configuration.chat_root_dir)
+    except exc.DatabaseError as db_error:
+        raise HTTPException(status_code=400, detail=db_error.detail)
+    except OSError as os_error:
+        raise HTTPException(status_code=400, detail=os_error.strerror)
+
+
+@router.delete("/{chat_id}")
+def delete_chat(chat_id: int, db: Session = Depends(get_db)):
+    try:
+        Chats.delete_chat(db, chat_id, configuration.chat_root_dir)
+    except exc.DatabaseError as e:
+        raise HTTPException(status_code=400, detail=e.detail)
+    except OSError as os_error:
+        raise HTTPException(status_code=400, detail=os_error.strerror)
+
+
+@router.put("/", response_model=Chat.Chat)
+def update_chat(chat: Chat.Chat, db: Session = Depends(get_db)):
+    try:
+        return Chats.update_chat(db, chat)
+    except exc.DatabaseError as e:
+        raise HTTPException(status_code=400, detail=e.detail)
+    except OSError as os_error:
+        raise HTTPException(status_code=400, detail=os_error.strerror)
+
+
+@router.get("/{chat_id}", response_model=Chat.Chat)
+def get_chat(chat_id: int, db: Session = Depends(get_db)):
+    try:
+        return Chats.get_chat(db, chat_id)
+    except exc.DatabaseError as e:
+        raise HTTPException(status_code=400, detail=e.detail)
+    except OSError as os_error:
+        raise HTTPException(status_code=400, detail=os_error.strerror)
+
+
+@router.get("/", response_model=list[Chat.Chat])
+def get_chats(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    try:
+        return Chats.get_chats(db, skip, limit)
+    except exc.DatabaseError as e:
+        raise HTTPException(status_code=400, detail=e.detail)
+    except OSError as os_error:
+        raise HTTPException(status_code=400, detail=os_error.strerror)
+
+
+@router.get("/offer/{offer_id}", response_model=Chat.Chat)
+def get_chat_by_offer(offer_id: int, db: Session = Depends(get_db)):
+    try:
+        return Chats.get_chat_by_offer(db, offer_id)
+    except exc.DatabaseError as e:
+        raise HTTPException(status_code=400, detail=e.detail)
+    except OSError as os_error:
+        raise HTTPException(status_code=400, detail=os_error.strerror)
+
+
+@router.get("/user/{user_id}", response_model=list[Chat.Chat])
+def get_chats_by_user(user_id: int, db: Session = Depends(get_db)):
+    try:
+        return Chats.get_chat_by_user(db, user_id)
+    except exc.DatabaseError as e:
+        raise HTTPException(status_code=400, detail=e.detail)
+    except OSError as os_error:
+        raise HTTPException(status_code=400, detail=os_error.strerror)
