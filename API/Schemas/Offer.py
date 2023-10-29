499be102d97ac16f37d74ffe9bf8d141c5eb353e
Robin Hassler <robin.edith74@gmail.com>
2023-10-29 17:09:31 +0100

    Added acceptance of null values for the subcategory id while adding an offer

--- API/Schemas/Offer.py
+++ API/Schemas/Offer.py
@@ -10,7 +10,7 @@ from pydantic import BaseModel, Field
 class OfferBase(BaseModel):
     title: str
     category_id: int
-    subcategory_id: int
+    subcategory_id: int | None = None
     price: float | None = Field(default=None, ge=0, description="The price must be greater then zero or None instead")
     currency: str = Field(default='€', max_length=1, description="The currency of the offer")
     postcode: str
